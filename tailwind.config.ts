aea80e5198e3e4b6b44597a4fa9cb8de7254636e
pntmernbatchthree <pntmernbatchthree@gmail.com>
2024-09-06 16:09:25 +0600

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Jost", ...defaultTheme.fontFamily.sans],
+        titleFont: ["Montserrat", "sans-serif"],
+        bodyFont: ["Roboto", "sans-serif"],
       },
     },
   },
