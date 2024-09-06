f7f3aac7ee19600b4abee754aca4087683345dcf
JibonRoy46 <jibonroy9320@gmail.com>
2024-09-06 23:01:21 +0600

    commit first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
 export default function Home() {
   return (
     <main className="p-20 max-w-screen-xl mx-auto flex flex-col gap-5">
-      <p>Noor Mohammad</p>
-      <p>Changes from test</p>
-      <p>Another Changes from test</p>
-      <h1> Jibon Roy</h1>
-      <h1>Md Golam Sarowar 2.0</h1>
-
-      <h1 className="text-2xl font-bold font-titleFont">
-        Lorem ipsum dolor sit amet.
-      </h1>
-      <h2 className="text-xl font-semibold font-bodyFont">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-        assumenda labore et culpa, pariatur animi!
-      </h2>
-      <h3 className="text-lg font-medium">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit hic quam
-        cumque ut. Iure minus itaque reiciendis earum praesentium officia iusto
-        aspernatur voluptatibus quibusdam blanditiis?
-      </h3>
-      <p className="text-base font-normal">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe nostrum
-        earum minus perferendis ad repellendus ex minima fugit, est iste
-        molestias, porro doloribus. Magni, deleniti ratione soluta autem nobis
-        temporibus vel deserunt. Optio neque qui dolores consequatur nobis earum
-        voluptatem recusandae, velit voluptates, iure, deleniti repellat
-        blanditiis officia? Commodi, dignissimos!
-      </p>
+      <div>
+        
+      </div>
     </main>
   );
 }
